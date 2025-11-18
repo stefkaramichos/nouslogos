7afd3b2c0cffa84ffdcf3817c1cd44b0b47bd76e
stefanos karamichos <stephanoskaramichos@gmail.com>
2025-11-18 19:12:23 +0200

    css changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
 
     <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 
     <style>
         body {
