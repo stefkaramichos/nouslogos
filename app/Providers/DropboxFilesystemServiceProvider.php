da4195fbebd736cc1058ea34e11dc8799a9cc7c0
stefanos karamichos <stephanoskaramichos@gmail.com>
2026-01-09 18:10:32 +0200

    dropbox2

--- app/Providers/DropboxFilesystemServiceProvider.php
+++ app/Providers/DropboxFilesystemServiceProvider.php
@@ -8,18 +8,39 @@ use Illuminate\Filesystem\FilesystemAdapter;
 use League\Flysystem\Filesystem;
 use Spatie\Dropbox\Client as DropboxClient;
 use Spatie\FlysystemDropbox\DropboxAdapter;
+use GuzzleHttp\Client as GuzzleClient;
 
 class DropboxFilesystemServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
         Storage::extend('dropbox', function ($app, $config) {
-            $client = new DropboxClient($config['token']);
-            $adapter = new DropboxAdapter($client);
 
+            // 1) Get a fresh short-lived access token using the refresh token
+            $http = new GuzzleClient();
+
+            $response = $http->post('https://api.dropboxapi.com/oauth2/token', [
+                'form_params' => [
+                    'grant_type' => 'refresh_token',
+                    'refresh_token' => $config['refresh_token'],
+                    'client_id' => $config['app_key'],
+                    'client_secret' => $config['app_secret'],
+                ],
+                'timeout' => 20,
+            ]);
+
+            $data = json_decode((string) $response->getBody(), true);
+
+            if (!isset($data['access_token'])) {
+                throw new \RuntimeException('Dropbox: could not obtain access_token from refresh_token.');
+            }
+
+            // 2) Create the Dropbox client with the fresh access token (string)
+            $client = new DropboxClient($data['access_token']);
+
+            $adapter = new DropboxAdapter($client);
             $filesystem = new Filesystem($adapter);
 
-            // Return Laravel's adapter (this is what Laravel expects)
             return new FilesystemAdapter($filesystem, $adapter, $config);
         });
     }
--- config/filesystems.php
+++ config/filesystems.php
@@ -61,9 +61,12 @@ return [
         ],
         'dropbox' => [
             'driver' => 'dropbox',
-            'token' => env('DROPBOX_ACCESS_TOKEN'),
+            'refresh_token' => env('DROPBOX_REFRESH_TOKEN'),
+            'app_key' => env('DROPBOX_APP_KEY'),
+            'app_secret' => env('DROPBOX_APP_SECRET'),
             'throw' => true,
         ],
+
 
 
     ],
